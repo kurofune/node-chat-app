aeb21c5cf6505f748914fa949db95f6ed804b38d
Nate Gross <nate@outcastgames.com>
2017-03-24 14:09:48 -0700

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generate message', () => {
     it('should generate the correct message object', () => {
@@ -10,5 +10,18 @@ describe('generate message', () => {
 
         expect(message.createdAt).toBeA('number');
         expect(message).toInclude({from, text});
+    });
+});
+
+describe('generate location message', () => {
+    it('should generate the correct location message object', () => {
+        var from = 'Nate';
+        var lat = 15;
+        var lng = 19;
+        var url = 'https://www.google.com/maps?q=15,19';
+        var message = generateLocationMessage(from, lat, lng);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
     });
 });
