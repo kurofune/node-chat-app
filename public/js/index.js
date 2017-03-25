f6d022d8bc9a7624aa5a51e8bed5dd44892ec815
Nate Gross <nate@outcastgames.com>
2017-03-24 17:44:16 -0700

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -9,32 +9,30 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('newMessage', message);
-    
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
-    console.log('newLocationMessage', message);
-
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-    from: 'Frank',
-    text: 'Hi'
-}, function (data) {
-    console.log('Got it!', data);
-});
+// socket.emit('createMessage', {
+//     from: 'Frank',
+//     text: 'Hi'
+// }, function (data) {
+//     console.log('Got it!', data);
+// });
 
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
